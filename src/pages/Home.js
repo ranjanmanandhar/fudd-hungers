c4f4e15da5b33d603bc550ee9119189069f76c26
Ranjan Manandhar <ranjan@192-168-1-4.tpgi.com.au>
2024-11-10 15:19:41 +1100

    temp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,20 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import { FaInstagram, FaBars } from "react-icons/fa";
 import Logo from "../assets/photos/logo-fudd.png";
-import { Link } from "react-router-dom";
-import "../styles/home.css";
 
 function Home() {
   return (
     <div className="home" id="/">
-      <Container
-        fluid
-        className="d-flex justify-content-center align-items-center min-vh-100"
-      >
-        {/* Use flexbox classes to center the content */}
-        <Row className="text-center">
-          <div className="logo mb-4">
-            <img src={Logo} alt="Logo" />
-          </div>
-
-          <div className="mb-4">
-            {/*  <a
-              href="https://www.instagram.com/yourprofile" // Replace with your Instagram profile link
-              target="_blank"
-              rel="noopener noreferrer"
-              className="instagram-icon mr-3"
-            >
-              <FaInstagram size={30} color="#E1306C" /> 
-            </a>
-            */}
-            <Link to="/menu" className="menu-link">
-              <FaBars size={30} color="#333" style={{ marginRight: "8px" }} />{" "}
-            </Link>
+      <Container className="d-flex justify-content-center">
+        <Row className="mb-4">
+          <div className="logo">
+            <img src={Logo} />
           </div>
         </Row>
+      </Container>
+
+      <Container fluid>
+        <div className="mb-4 text-center"></div>
       </Container>
     </div>
   );
