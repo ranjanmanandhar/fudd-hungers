4879ec3fa945d9e8e3cf47f9aabd70c3c99b2cee
Ranjan Manandhar <ranjan@192-168-1-4.tpgi.com.au>
2024-11-10 16:15:29 +1100

    add png

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import Logo from "../assets/photos/logo-fudd.png";
+import { FaInstagram, FaBars } from "react-icons/fa"; // Importing Instagram and Menu icons
+import { Link } from "react-router-dom"; // Importing Link for navigation
+import Logo from "../assets/photos/logo-fudd.png"; // Assuming you have the logo image in this path
 
 function Home() {
   return (
@@ -8,13 +10,31 @@ function Home() {
       <Container className="d-flex justify-content-center">
         <Row className="mb-4">
           <div className="logo">
-            <img src={Logo} />
+            <img src={Logo} alt="Logo" />
           </div>
         </Row>
       </Container>
 
       <Container fluid>
-        <div className="mb-4 text-center"></div>
+        <div className="mb-4 text-center">
+          {/* Instagram icon with link */}
+          <a
+            href="https://www.instagram.com/ranjan_mndr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram size={50} color="black" />{" "}
+            {/* Change color as needed */}
+          </a>
+
+          {/* Menu icon to go to /menu */}
+          <div className="mt-4">
+            <Link to="/menu" className="menu-icon">
+              <FaBars size={40} color="black" />{" "}
+              {/* Adjust size and color as needed */}
+            </Link>
+          </div>
+        </div>
       </Container>
     </div>
   );
