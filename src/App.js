581349f224bf57d64595d93ebeaba1bfb7e0a582
Ranjan Manandhar <ranjan@192-168-1-4.tpgi.com.au>
2024-11-10 16:01:50 +1100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 
@@ -9,8 +9,8 @@ const App = () => {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={Menu} />
-          <Route path="/menu" element={Menu} />
+          <Route path="/" element={<Menu />} />
+          <Route path="/menu" element={<Menu />} />
         </Routes>
       </Router>
     </>
